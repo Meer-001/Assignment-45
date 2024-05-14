896813259c88d0fd6a3dfe01b916b955d26f8c35
Ahmer Shaikh <167479939+Meer-001@users.noreply.github.com>
2024-05-14 08:16:51 +0500

    Add files via upload

---
+++ Question (3)/main.ts
@@ -0,0 +1,19 @@
+//____________________________________________________________________________________
+//                            * Question No 3 *
+//____________________________________________________________________________________
+
+
+let personName = "shahmir shaikh"
+
+console.log(personName.toLowerCase());
+
+console.log(personName.toUpperCase());
+
+console.log(personName.replace(/\b\w/g, (abc) => abc.toUpperCase()));
+
+
+////-----------------------------------
+// Result 3 = shahmir shaikh
+//            SHAHMIR SHAIKH
+//            Shahmir Shaikh
+////-----------------------------------
