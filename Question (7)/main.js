6f9cafd062604bcee08a6d943363892cd11c4f85
Ahmer Shaikh <167479939+Meer-001@users.noreply.github.com>
2024-05-14 08:18:10 +0500

    Add files via upload

---
+++ Question (7)/main.js
@@ -0,0 +1,25 @@
+//____________________________________________________________________________________
+//                            * Question No 7 *
+//____________________________________________________________________________________
+// (1) Addition
+var sum1 = 6;
+var sum2 = 2;
+console.log(sum1 + sum2);
+// (2) Substraction
+var sum3 = 10;
+var sum4 = 2;
+console.log(sum3 - sum4);
+// Multiplication
+var sum5 = 4;
+var sum6 = 2;
+console.log(sum5 * sum6);
+// division 
+var sum7 = 16;
+var sum8 = 2;
+console.log(sum7 / sum8);
+////---------------------
+// Result 7 = 8
+//            8
+//            8
+//            8
+////---------------------
---
+++ Question (7)/main.ts
@@ -0,0 +1,32 @@
+//____________________________________________________________________________________
+//                            * Question No 7 *
+//____________________________________________________________________________________
+
+// (1) Addition
+let sum1:number = 6;
+let sum2:number = 2;
+console.log(sum1 + sum2);
+
+// (2) Substraction
+let sum3:number = 10;
+let sum4:number = 2;
+console.log(sum3 - sum4);
+
+
+// Multiplication
+let sum5:number = 4;
+let sum6:number = 2;
+console.log(sum5 * sum6);
+
+
+// division 
+let sum7:number = 16;
+let sum8:number = 2;
+console.log(sum7 / sum8);
+
+////---------------------
+// Result 7 = 8
+//            8
+//            8
+//            8
+////---------------------
